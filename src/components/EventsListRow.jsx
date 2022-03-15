144734da097a93fa368f41f7e5718bc9e70308e5
robertlacheta <robert.lacheta@gmail.com>
2022-03-15 04:34:16 +0100

    deleted selector

--- src/components/EventsListRow.jsx
+++ src/components/EventsListRow.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 
 const EventsListRow = ({ id, title, category, date, time, city }) => {
-  const events = useSelector((state) => state);
-
   const dispatch = useDispatch();
 
   const deleteEvent = (id) => {
